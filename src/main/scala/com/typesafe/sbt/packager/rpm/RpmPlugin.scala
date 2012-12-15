7d2a1e22506f49c2a96ebba8771885e60dcb9921
Felix Leipold <felix.leipold@gmail.com>
2012-12-15 13:15:12 +0100

    Revert "Defaulting seems to cause problems with description. Which is now empty."
    This reverts commit c6e4f475abe8f3a8d07342915ed7e6c7cd9d87ca.

--- src/main/scala/com/typesafe/sbt/packager/rpm/RpmPlugin.scala
+++ src/main/scala/com/typesafe/sbt/packager/rpm/RpmPlugin.scala
@@ -12,6 +12,8 @@ trait RpmPlugin extends Plugin with LinuxPlugin {
   
   def rpmSettings: Seq[Setting[_]] = Seq(
     rpmOs := "Linux",  // TODO - default to something else?
+    rpmRelease := "0",
+    rpmVendor := "",  // TODO - Maybe pull in organization?
     rpmLicense := None,
     rpmDistribution := None,
     rpmUrl := None,
@@ -31,6 +33,7 @@ trait RpmPlugin extends Plugin with LinuxPlugin {
     rpmPreun := None,
     rpmPostun := None,
     packageSummary in Rpm <<= packageSummary in Linux,
+    packageDescription in Rpm <<= packageDescription in Linux,
     target in Rpm <<= target(_ / "rpm")
   ) ++ inConfig(Rpm)(Seq(
     packageArchitecture := "noarch",
